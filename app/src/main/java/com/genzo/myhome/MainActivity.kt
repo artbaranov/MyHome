80c38d787da2757dcf44f14e753cb52d35514e1f
artembaranov <artbaranov@storytelling-software.com>
2023-09-02 17:00:19 +0300

    Removed generated code

--- app/src/main/java/com/genzo/myhome/MainActivity.kt
+++ app/src/main/java/com/genzo/myhome/MainActivity.kt
@@ -15,32 +15,5 @@ import com.genzo.myhome.ui.theme.MyHomeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyHomeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyHomeTheme {
-        Greeting("Android")
     }
 }
