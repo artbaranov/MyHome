639539a37ac1926c2136c46a3be009e3b873912d
artembaranov <artbaranov@storytelling-software.com>
2023-09-03 14:42:25 +0300

    Renamed `MainActivity` to the `RootActivity`

---
+++ app/src/main/java/com/genzo/myhome/RootActivity.kt
@@ -0,0 +1,10 @@
+package com.genzo.myhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class RootActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
