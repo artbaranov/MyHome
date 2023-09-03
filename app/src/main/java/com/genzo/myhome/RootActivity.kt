ad5ac5c870ee308752b07c89796f6f151d55e806
artembaranov <artbaranov@storytelling-software.com>
2023-09-03 14:46:24 +0300

    Implemented home screen display

--- app/src/main/java/com/genzo/myhome/RootActivity.kt
+++ app/src/main/java/com/genzo/myhome/RootActivity.kt
@@ -2,9 +2,30 @@ package com.genzo.myhome
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.genzo.myhome.ui.navigation.Navigation
+import com.genzo.myhome.ui.navigation.navGraph
+import com.genzo.myhome.ui.theme.MyHomeTheme
 
 class RootActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        setContent {
+            MyHomeTheme {
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Navigation.HOME.ROOT
+                ) {
+                    navGraph(
+                        navController = navController,
+                    )
+                }
+            }
+        }
     }
 }
