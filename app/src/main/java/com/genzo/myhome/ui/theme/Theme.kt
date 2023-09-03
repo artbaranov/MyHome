dc8a1baca44730d04cda90b2946df102af79931b
artembaranov <artbaranov@storytelling-software.com>
2023-09-03 14:45:52 +0300

    Added typography style

--- app/src/main/java/com/genzo/myhome/ui/theme/Theme.kt
+++ app/src/main/java/com/genzo/myhome/ui/theme/Theme.kt
@@ -9,11 +9,14 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.compose.material3.Typography
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -25,17 +28,14 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
+
+object MyHomeTheme {
+    val typography: Typography
+        @Composable
+        @ReadOnlyComposable
+            get() = MyHomeTypography
+}
 
 @Composable
 fun MyHomeTheme(
@@ -64,7 +64,7 @@ fun MyHomeTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = MyHomeTheme.typography,
         content = content
     )
 }
---
+++ app/src/main/java/com/genzo/myhome/ui/theme/Typography.kt
@@ -0,0 +1,27 @@
+package com.genzo.myhome.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.genzo.myhome.R
+
+private val Circe = FontFamily(
+    Font(R.font.circe_regular, FontWeight.Normal),
+    Font(R.font.circe_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.circe_bold, FontWeight.Bold),
+    Font(R.font.circe_light, FontWeight.Light),
+    Font(R.font.circe_thin, FontWeight.Thin),
+)
+
+private val LargeTitle = TextStyle(
+    fontFamily = Circe,
+    fontWeight = FontWeight.Light,
+    fontSize = 21.sp
+)
+
+val MyHomeTypography = Typography(
+    titleLarge = LargeTitle
+)
