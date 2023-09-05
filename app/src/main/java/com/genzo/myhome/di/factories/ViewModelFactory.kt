c9af118aae70391ac0df7ba160d5e3e03c06867a
artembaranov <artbaranov@storytelling-software.com>
2023-09-05 15:54:48 +0300

    Implemented `DoorsViewModelFactory`

--- app/src/main/java/com/genzo/myhome/di/factories/ViewModelFactory.kt
+++ app/src/main/java/com/genzo/myhome/di/factories/ViewModelFactory.kt
@@ -2,7 +2,9 @@ package com.genzo.myhome.di.factories
 
 import androidx.lifecycle.ViewModel
 import com.genzo.myhome.data.repositories.CamerasRepository
+import com.genzo.myhome.data.repositories.DoorsRepository
 import com.genzo.myhome.ui.sections.cameras.viewModel.CamerasViewModel
+import com.genzo.myhome.ui.sections.doors.viewModel.DoorsViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 
 interface ViewModelFactory {
@@ -17,6 +19,20 @@ class CamerasViewModelFactory(
     override fun create(): CamerasViewModel {
         return CamerasViewModel(
             camerasRepository,
+            uiDispatcher,
+            ioDispatcher
+        )
+    }
+}
+
+class DoorsViewModelFactory(
+    private val doorsRepository: DoorsRepository,
+    private val uiDispatcher: CoroutineDispatcher,
+    private val ioDispatcher: CoroutineDispatcher
+) : ViewModelFactory {
+    override fun create(): DoorsViewModel {
+        return DoorsViewModel(
+            doorsRepository,
             uiDispatcher,
             ioDispatcher
         )
