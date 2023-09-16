ac4cc49055e33e5c8806bdc457da4662d5ac3c87
artembaranov <artbaranov@storytelling-software.com>
2023-09-16 11:49:35 +0300

    Implemented db entities for `Camera` and `Door`

---
+++ app/src/main/java/com/genzo/myhome/data/repositories/enitities/Door.kt
@@ -0,0 +1,15 @@
+package com.genzo.myhome.data.repositories.enitities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Door(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val title: String,
+    val snapshot: String,
+    val favorites: Boolean,
+    val locked: Boolean,
+    val room: String,
+)
