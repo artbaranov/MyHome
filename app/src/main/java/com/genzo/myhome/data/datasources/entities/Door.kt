0f9f1f4868ce887d7a498df488fee028b6e6be5b
artembaranov <artbaranov@storytelling-software.com>
2023-09-16 17:55:10 +0300

    Made room parameter optional

--- app/src/main/java/com/genzo/myhome/data/datasources/entities/Door.kt
+++ app/src/main/java/com/genzo/myhome/data/datasources/entities/Door.kt
@@ -2,8 +2,8 @@ package com.genzo.myhome.data.datasources.entities
 
 data class Door(
     val name: String,
-    val room: String,
-    val id: Int,
+    val room: String?,
+    val id: Long,
     val favorites: Boolean,
     val snapshot: String,
 )
