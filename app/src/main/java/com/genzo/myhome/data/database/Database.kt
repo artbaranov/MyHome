af0d800792e9d41731f52ccebe87a30c5c87da6a
artembaranov <artbaranov@storytelling-software.com>
2023-09-16 18:00:39 +0300

    Migrated database to 2 version

--- app/src/main/java/com/genzo/myhome/data/database/Database.kt
+++ app/src/main/java/com/genzo/myhome/data/database/Database.kt
@@ -1,11 +1,20 @@
 package com.genzo.myhome.data.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.genzo.myhome.data.database.dao.CamerasDao
+import com.genzo.myhome.data.database.dao.DoorsDao
 import com.genzo.myhome.data.repositories.enitities.Camera
+import com.genzo.myhome.data.repositories.enitities.Door
 
-@Database(entities = [Camera::class], version = 1)
+@Database(
+    entities = [Camera::class, Door::class],
+    version = 2,
+    exportSchema = true,
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
+)
 abstract class Database : RoomDatabase() {
     abstract fun camerasDao(): CamerasDao
+    abstract fun doorsDao(): DoorsDao
 }
