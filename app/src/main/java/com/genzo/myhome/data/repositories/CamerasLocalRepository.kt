1cb075ea88d53ec983f0f8f4c26bc5e809d3365b
artembaranov <artbaranov@storytelling-software.com>
2023-09-16 16:50:17 +0300

    Implemented getting all cameras from db

--- app/src/main/java/com/genzo/myhome/data/repositories/CamerasLocalRepository.kt
+++ app/src/main/java/com/genzo/myhome/data/repositories/CamerasLocalRepository.kt
@@ -3,15 +3,25 @@ package com.genzo.myhome.data.repositories
 import com.genzo.myhome.data.database.dao.CamerasDao
 import com.genzo.myhome.data.database.mappers.CameraMapper
 import com.genzo.myhome.data.datasources.entities.Camera
+import javax.inject.Inject
 
 interface CamerasLocalRepository {
     suspend fun insertCamera(camera: Camera)
+    suspend fun getAllCameras(): List<Camera>
 }
 
-class CamerasLocalRepositoryImpl(private val camerasDao: CamerasDao) : CamerasLocalRepository {
+class CamerasLocalRepositoryImpl @Inject constructor(private val camerasDao: CamerasDao) : CamerasLocalRepository {
     override suspend fun insertCamera(camera: Camera) {
         val dbCamera = CameraMapper.map(camera)
         camerasDao.insert(dbCamera)
+    }
+
+    override suspend fun getAllCameras(): List<Camera> {
+        val dbCameras = camerasDao.getAll()
+
+        return dbCameras.map {
+            CameraMapper.map(it)
+        }
     }
 }
 
