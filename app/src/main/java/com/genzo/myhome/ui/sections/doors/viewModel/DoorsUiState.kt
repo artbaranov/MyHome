dacd6b07e40f7eee557e753cd9f4c3dd1d2dc0f6
artembaranov <artbaranov@storytelling-software.com>
2023-09-26 14:56:04 +0300

    Removed Room entity

--- app/src/main/java/com/genzo/myhome/ui/sections/doors/viewModel/DoorsUiState.kt
+++ app/src/main/java/com/genzo/myhome/ui/sections/doors/viewModel/DoorsUiState.kt
@@ -1,10 +1,8 @@
 package com.genzo.myhome.ui.sections.doors.viewModel
 
 import com.genzo.myhome.data.datasources.entities.Door
-import com.genzo.myhome.data.datasources.entities.Room
 
 data class DoorsUiState(
-    val rooms: List<Room> = emptyList(),
     val standaloneDoors: List<Door> = emptyList(),
     val editNameDialogVisible: Boolean = false,
     val doorName: String = "",
